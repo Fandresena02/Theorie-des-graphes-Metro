bc061ce183536b34b46062804c816f8120719397
sophieminos <87713100+sophieminos@users.noreply.github.com>
2022-10-23 13:40:58 +0200

    Update parcours.py

--- parcours.py
+++ parcours.py
@@ -40,13 +40,20 @@ def lire_fichier_sommets(nom_fichier):
     # with open(nom_fichier, "r") as filin :
     for ligne in nom_fichier:
         lst = ligne.split(";")
-        dico = {"numero_sommet" : int(lst[1]), "nom_sommet" : lst[2].strip(), "ligne" : lst[3], "terminus" : lst[4], "branchement" : int(lst[5])}
+        dico = {"numero_sommet" : int(lst[1]), "nom_sommet" : lst[2].strip(), "ligne" : lst[3].strip(), "terminus" : lst[4], "branchement" : int(lst[5])}
         lst_f.append(dico)
     return lst_f
 
 stations=lire_fichier_sommets(fichier_sommets)
 
 voisins_sommets=lire_fichier_aretes(fichier_aretes)
+voisins_sommets_keys = sorted(voisins_sommets.keys())
+
+voisins_sommets_tries = {}
+for key in voisins_sommets_keys:
+    voisins_sommets_tries[key] = voisins_sommets[key]
+
+#print(voisins_sommets_tries)
 
 def is_station_in_chemin(chemin, num_station):
     for station in chemin:
@@ -75,19 +82,19 @@ def algo_durees_min(depart):
     num_dep=0
 
     #initialisation du tableau de distances_min
-    for sommet in voisins_sommets:
-        #print(list(voisins_sommets.values())[station-1])
+    for sommet in voisins_sommets_tries:
+        #print(list(voisins_sommets_tries.values())[station-1])
         #print(sommet)
         durees_min[sommet]=None
         peres[sommet]=None
     
+    
     #definition du numéro de départ
     for station in stations:
-        print(station['nom_sommet'])
-        if station['nom_sommet'].__eq__(depart): 
-            num_dep=station['numero_sommet']    
+        #print(station['nom_sommet'])
+        if station['nom_sommet']==depart: num_dep=station['numero_sommet']    
     
-    print(num_dep)
+    #print(num_dep)
     # boucle jusqu'à trouver la station
     station_courante=[num_dep,0]
     durees_min[num_dep]=station_courante[1]
@@ -95,22 +102,30 @@ def algo_durees_min(depart):
     
     while len(chemin)<len(stations):
         chemin.append(station_courante);
-        for sommet in voisins_sommets:
+        for sommet in voisins_sommets_tries:
             
             if sommet==station_courante[0]:
-                #print(list(voisins_sommets.values())[sommet])
-                for voisin in list(voisins_sommets.values())[sommet]:
-                    
-                    addition_durees=durees_min[sommet]+list(voisins_sommets.values())[sommet][voisin]
+                #print(list(voisins_sommets_tries.values())[sommet])
+                for voisin in list(voisins_sommets_tries.values())[sommet]:
+                    #print(voisin)
+                    addition_durees=durees_min[sommet]+list(voisins_sommets_tries.values())[sommet][voisin]
                     
                     if (is_station_in_chemin(chemin, voisin)==0) or \
                         (durees_min[voisin]==None) or \
                         (addition_durees < durees_min[voisin]):
-                        #print(durees_min)
+                        
+                        #print(get_station(sommet)['nom_sommet'],'===>',get_station(voisin)['nom_sommet'])
+                        #if(durees_min[voisin]==None): 
+                        #    print('durees_min[', voisin,'] == none')
+                        #elif(addition_durees < durees_min[voisin]): 
+                        #    #print(addition_durees, '<', durees_min[voisin],'==',(addition_durees < durees_min[voisin]))
+                        
                         #print(voisin)
                         if durees_min[voisin]==None: stations_vues.append(voisin)
                     
                         durees_min[voisin]=addition_durees
+                        #print('durees_min[', voisin,'] ==', durees_min[voisin], '\n')
+                        
                         peres[voisin]=sommet
                         
         station_courante=station_duree_min(durees_min, stations_vues, chemin)
@@ -126,7 +141,8 @@ def get_station(num_station):
 def get_ligne_lien(station1, station2):
     stations1=[]
     stations2=[]
-
+    #print(station1)
+    #print(station2)
     # implémentation des tableaux
     for station in stations:
         if station['nom_sommet']==station1['nom_sommet']: stations1.append(station)
@@ -180,21 +196,25 @@ def parcours_chemin(durees_min, peres, depart, arrivee):
             print('Prenez la ligne', get_ligne_lien(station_precedente, station))
         
         #indication de changement de ligne   
-        if station['numero_sommet']!=num_dep and station_precedente['ligne']!=station['ligne']:
+        if station['numero_sommet']!=num_dep and station_precedente['ligne']!=station['ligne'] and station['numero_sommet']!=num_arr:
             print('A', station['nom_sommet'], 'changez et prenez la ligne ', station['ligne'])
         
         #arrivee
         if station['numero_sommet']==num_arr: 
-            print('Vous devriez arriver à', station['nom_sommet'], 'dans environ', durees_min[station['numero_sommet']], 'minutes')
+            print('Vous devriez arriver à', station['nom_sommet'], 'dans environ', round((durees_min[station['numero_sommet']]/60),2), 'minutes')
         
         station_precedente=station
 
-depart='Place Monge'
-arrivee='Place d\'Italie'
+depart='Carrefour Pleyel'
+arrivee='Rue Montmartre, Grands Boulevards'
 
 durees_min,peres=algo_durees_min(depart)
 
 parcours_chemin(durees_min, peres, depart, arrivee)
+
+#print('\nPPC:')
+#for station in durees_min:
+#    print(get_station(peres[station])['nom_sommet'], '->', get_station(station)['nom_sommet'], '\t tps:', durees_min[station]/60, 'min')
 
 fichier_aretes.close()
 fichier_sommets.close()
