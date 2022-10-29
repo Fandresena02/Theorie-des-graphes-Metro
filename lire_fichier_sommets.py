d0f3feb3d984ddb8c3e69774e83ff87a010c19b9
christine-jl <76869239+christine-jl@users.noreply.github.com>
2022-10-29 16:23:26 +0200

    Update lire_fichier_sommets.py

--- lire_fichier_sommets.py
+++ lire_fichier_sommets.py
@@ -1,10 +1,11 @@
 def fichier_sommet(nom_fichier):
     lst_f = []
-    # with open(nom_fichier, "r") as filin :
+    
     for ligne in nom_fichier:
         lst = ligne.split(";")
         dico = {"numero_sommet" : int(lst[1]), "nom_sommet" : lst[2], "ligne" : lst[3], "terminus" : lst[4], "branchement" : int(lst[5])}
         lst_f.append(dico)
+        
     return lst_f
 
 
