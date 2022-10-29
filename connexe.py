fad7d6e5fb8486257b74a8f44593a8a8d695a0ab
christine-jl <76869239+christine-jl@users.noreply.github.com>
2022-10-29 16:22:46 +0200

    Update connexe.py

--- connexe.py
+++ connexe.py
@@ -2,19 +2,19 @@ import lire_fichier_sommets
 import lire_fichier_aretes
 
 
-def exist_chemin(matrice, nombre_sommets, u, v):
+def chemin_existe(matrice, nombre_sommets, u, v):
     #nombre_sommets = len(lire_fichier_sommets.fichier_sommet(fichier))
     # matrice = lire_fichier_aretes.fichier_arete(fichier1)
     file = []
-    visites = [False] * nombre_sommets
+    sommets_visites = [False] * nombre_sommets
     file.append(u)
     while(file):
         actuel = file.pop(0)
-        visites[actuel] = True
+        sommets_visites[actuel] = True
         for i in range(nombre_sommets):
-            if matrice.get(actuel, {}).get(i) and visites[i] == False:
+            if matrice.get(actuel, {}).get(i) and sommets_visites[i] == False:
                 file.append(i)
-                visites[i] = True
+                sommets_visites[i] = True
                 if i == v:
                     return True
             elif matrice.get(actuel, {}).get(i) and (i == v):
@@ -28,8 +28,8 @@ def connexe(fichier, fichier1):
     for i in range(nombre_sommets):
         for j in range(i+1, nombre_sommets):
             if i != j:
-                # print(exist_chemin(matrice, nombre_sommets, i, j), i, j)
-                if exist_chemin(matrice, nombre_sommets, i, j) == False:
+                # print(chemin_existe(matrice, nombre_sommets, i, j), i, j)
+                if chemin_existe(matrice, nombre_sommets, i, j) == False:
                     return False
     return True
 
@@ -37,7 +37,7 @@ def connexe(fichier, fichier1):
 fichier = open("sommets.txt", 'r')
 fichier1 = open("aretes.txt", 'r')
 print(connexe(fichier, fichier1))
-# print(exist_chemin(lire_fichier_aretes.fichier_arete(fichier1),
+# print(chemin_existe(lire_fichier_aretes.fichier_arete(fichier1),
 #       len(lire_fichier_sommets.fichier_sommet(fichier)), 1, 364))
 fichier.close()
 fichier1.close()
