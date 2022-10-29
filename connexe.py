1804d69a0039d1172f2a6e42d6dc31de7e69e95f
christine-jl <76869239+christine-jl@users.noreply.github.com>
2022-10-29 17:32:04 +0200

    Update connexe.py

--- connexe.py
+++ connexe.py
@@ -2,19 +2,19 @@ import lire_fichier_sommets
 import lire_fichier_aretes
 
 
-def chemin_existe(matrice, nombre_sommets, u, v):
+def exist_chemin(matrice, nombre_sommets, u, v):
     #nombre_sommets = len(lire_fichier_sommets.fichier_sommet(fichier))
     # matrice = lire_fichier_aretes.fichier_arete(fichier1)
     file = []
-    sommets_visites = [False] * nombre_sommets
+    visites = [False] * nombre_sommets
     file.append(u)
     while(file):
         actuel = file.pop(0)
-        sommets_visites[actuel] = True
+        visites[actuel] = True
         for i in range(nombre_sommets):
-            if matrice.get(actuel, {}).get(i) and sommets_visites[i] == False:
+            if matrice.get(actuel, {}).get(i) and visites[i] == False:
                 file.append(i)
-                sommets_visites[i] = True
+                visites[i] = True
                 if i == v:
                     return True
             elif matrice.get(actuel, {}).get(i) and (i == v):
@@ -23,21 +23,19 @@ def chemin_existe(matrice, nombre_sommets, u, v):
 
 
 def connexe(fichier, fichier1):
-    nombre_sommets = len(lire_fichier_sommets.fichier_sommet(fichier))
-    matrice = lire_fichier_aretes.fichier_arete(fichier1)
+    nombre_sommets = len(lire_fichier_sommets.fichier_sommets(fichier))
+    matrice = lire_fichier_aretes.fichier_aretes(fichier1)
     for i in range(nombre_sommets):
         for j in range(i+1, nombre_sommets):
             if i != j:
-                # print(chemin_existe(matrice, nombre_sommets, i, j), i, j)
-                if chemin_existe(matrice, nombre_sommets, i, j) == False:
+                # print(exist_chemin(matrice, nombre_sommets, i, j), i, j)
+                if exist_chemin(matrice, nombre_sommets, i, j) == False:
                     return False
     return True
 
-
+#TESTS
 fichier = open("sommets.txt", 'r')
 fichier1 = open("aretes.txt", 'r')
 print(connexe(fichier, fichier1))
-# print(chemin_existe(lire_fichier_aretes.fichier_arete(fichier1),
-#       len(lire_fichier_sommets.fichier_sommet(fichier)), 1, 364))
 fichier.close()
 fichier1.close()
